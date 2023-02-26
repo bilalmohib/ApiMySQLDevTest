980f4fdf97d82626d47bd13e58623e13057cfd6d
Muhammad-Bilal-7896 <2019cs682@student.uet.edu.pk>
2023-02-26 09:40:23 +0500

    Added

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,15 @@
 'use strict';
 const mysql = require('mysql');
+const express = require('express');
+const app = express();
+
+// Enable CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 //local mysql db connection
 const dbConn = mysql.createConnection({
   host     : 'eu-cdbr-west-03.cleardb.net',
@@ -7,8 +17,10 @@ const dbConn = mysql.createConnection({
   password : '3edef4cd',
   database : 'heroku_624aae288a3b296'
 });
+
 dbConn.connect(function(err) {
   if (err) throw err;
   console.log("Database Connected!");
 });
+
 module.exports = dbConn;
