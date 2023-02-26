f7e3a0be54f0bb1d61739260dfa96178bd916294
Muhammad-Bilal-7896 <2019cs682@student.uet.edu.pk>
2023-02-26 15:28:11 +0500

    Quiz API developing

--- server.js
+++ server.js
@@ -5,9 +5,12 @@ const app = express();
 // Setup server port
 const port = process.env.PORT || 3000;
 // Enable CORS
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 // parse requests of content-type - application/x-www-form-urlencoded
@@ -16,12 +19,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // define a root route
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.sendFile(__dirname + "/index.html");
 });
 // Require employee routes
 const quizRoutes = require("./src/routes/quiz.routes");
 // using as middleware
-app.use('/quiz', quizRoutes);
+app.use("/quiz", quizRoutes);
 // listen for requests
 app.listen(port, () => {
   console.log(`Quiz API started. Server is listening on port ${port}`);
