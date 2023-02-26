2a2a2654542e4815836df395528e05139059f0cd
Muhammad-Bilal-7896 <2019cs682@student.uet.edu.pk>
2023-02-26 16:24:18 +0500

    Quiz API developing

--- src/models/quiz.model.js
+++ src/models/quiz.model.js
@@ -1,45 +1,56 @@
 "use strict";
-var dbConn = require("./../../config/db.config");
-//Quiz object create
-var Quiz = function (quiz) {
-  this.title = quiz.title;
-  this.description = quiz.description;
-  this.quizQuestions = quiz.quizQuestions;
-  this.isActivated = quiz.isActivated;
-  this.created_by = quiz.created_by;
-  this.created_at = new Date();
-  this.updated_at = new Date();
-};
-Quiz.create = function (newEmp, result) {
-  dbConn.query("INSERT INTO quiz set ?", newEmp, function (err, res) {
-    if (err) {
-      console.log("error: ", err);
-      result(err, null);
-    } else {
-      console.log(res.insertId);
-      result(null, res.insertId);
-    }
-  });
-};
-Quiz.findById = function (id, result) {
-  dbConn.query(
-    "Select * from quiz where id = ? ",
-    id,
-    function (err, res) {
+
+const dbConn = require("./../../config/db.config");
+
+// Quiz object create
+class Quiz {
+  constructor(quiz) {
+    this.title = quiz.title;
+    this.description = quiz.description;
+    this.quizQuestions = quiz.quizQuestions;
+    this.isActivated = quiz.isActivated;
+    this.created_by = quiz.created_by;
+    this.created_at = new Date();
+    this.updated_at = new Date();
+  }
+
+  static create(newQuiz, result) {
+    const sql = "INSERT INTO quiz SET ?";
+    const quizData = {
+      title: newQuiz.title,
+      description: newQuiz.description,
+      quizQuestions: JSON.stringify(newQuiz.quizQuestions),
+      isActivated: newQuiz.isActivated,
+      created_by: newQuiz.created_by,
+      created_at: newQuiz.created_at,
+      updated_at: newQuiz.updated_at,
+    };
+    dbConn.query(sql, quizData, function (err, res) {
       if (err) {
         console.log("error: ", err);
         result(err, null);
       } else {
-        result(null, res);
+        console.log(res.insertId);
+        result(null, res.insertId);
       }
-    }
-  );
-};
-Quiz.findAll = function (result) {
-  dbConn.query("Select * from quiz", function (err, res) {
+    });
+  }
+}
+Quiz.findById = function (id, result) {
+  dbConn.query("SELECT * FROM quiz WHERE id = ?", [id], function (err, res) {
     if (err) {
       console.log("error: ", err);
-      result(null, err);
+      result(err, null);
+    } else {
+      result(null, res);
+    }
+  });
+};
+Quiz.findAll = function (result) {
+  dbConn.query("SELECT * FROM quiz", function (err, res) {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
     } else {
       console.log("quizs : ", res);
       result(null, res);
